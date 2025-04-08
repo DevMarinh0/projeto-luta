77542e4442c4daddf65fcd3e0bfdfe589c818979
Bruno Marinho <brunomarinhosouza16@gmail.com>
2025-04-08 03:41:38 -0300

    adicionando a parte de dano dos lutadores

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -61,9 +61,62 @@ class cenario {
   }
   comecarLuta() {
     this.atualizar();
+    //Atacar
+    this.lutador1Element
+      .querySelector(".attackButton")
+      .addEventListener("click", () =>
+        this.fazerAttack(this.lutador1, this.lutador2)
+      );
+    //Atacar
+    this.lutador2Element
+      .querySelector(".attackButton")
+      .addEventListener("click", () =>
+        this.fazerAttack(this.lutador2, this.lutador1)
+      );
   }
   atualizar() {
-    this.lutador1Element.querySelector(".name").innerHTML = this.lutador1.nome;
-    this.lutador2Element.querySelector(".name").innerHTML = this.lutador2.nome;
+    //lutador 1
+    this.lutador1Element.querySelector(
+      ".name"
+    ).innerHTML = `${this.lutador1.nome} -  ${this.lutador1.life} HP`;
+    let l1Porcent = (this.lutador1.life / this.lutador1.maxLife) * 100;
+    this.lutador1Element.querySelector(".bar").style.width = `${l1Porcent}%`;
+    //lutador 2
+    this.lutador2Element.querySelector(
+      ".name"
+    ).innerHTML = `${this.lutador2.nome} -  ${this.lutador2.life} HP`;
+    let l2Porcent = (this.lutador2.life / this.lutador2.maxLife) * 100;
+    this.lutador2Element.querySelector(".bar").style.width = `${l2Porcent}%`;
+  }
+  fazerAttack(atacando, atacado) {
+    //se alguem morrer, não faz nada
+    if (atacando.life <= 0 || atacado.life <= 0) {
+      console.log("Luta encerrada!");
+      return;
+    }
+
+    let ataqueFactor = (Math.random() * 1).toFixed(2);
+    let defesaFactor = (Math.random() * 1).toFixed(2); // 0.5 a 1.5
+
+    let ataqueAtual = atacando.attack * ataqueFactor;
+    let defesaAtual = atacado.defense * defesaFactor;
+
+    if (ataqueAtual > defesaAtual) {
+      let dano = ataqueAtual - defesaAtual;
+      atacado.life -= dano;
+      console.log(
+        `${atacando.nome} atacou ${atacado.nome} e causou ${dano.toFixed(
+          2
+        )} de dano`
+      );
+    } else {
+      console.log(
+        `${atacando.nome} conseguiu Defender o ataque de ${atacado.nome}`
+      );
+    }
+    console.log(
+      `${atacando.nome} atacou ${atacado.nome} com ${atacando.attack} de ataque`
+    );
+    this.atualizar();
   }
 }
